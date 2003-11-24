e1c82c2870ed18e6d4f6e12badf6238f81100c4c
pelle <pelle>
2003-11-24 23:37:33 +0000

    Testcase for SOAPServlet

---
+++ src/test-cactus/org/neuclear/xml/soap/SOAPServletTest.java
@@ -0,0 +1,57 @@
+package org.neuclear.xml.soap;
+
+import org.apache.cactus.ServletTestCase;
+import org.apache.cactus.WebRequest;
+import org.apache.cactus.WebResponse;
+import org.dom4j.DocumentHelper;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+/*
+NeuClear Distributed Transaction Clearing Platform
+(C) 2003 Pelle Braendgaard
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+$Id: SOAPServletTest.java,v 1.1 2003/11/24 23:37:33 pelle Exp $
+$Log: SOAPServletTest.java,v $
+Revision 1.1  2003/11/24 23:37:33  pelle
+Testcase for SOAPServlet
+
+*/
+
+/**
+ * User: pelleb
+ * Date: Nov 24, 2003
+ * Time: 4:39:55 PM
+ */
+public class SOAPServletTest extends ServletTestCase{
+/*
+    public void beginSoapRequest(WebRequest theRequest){
+        theRequest.setContentType("text/xml");
+        DocumentHelper.
+        theRequest.
+    }
+    public void testSoapRequest() throws ServletException, IOException {
+       EchoSOAPServlet servlet=new EchoSOAPServlet();
+        servlet.init(config);
+        servlet.doPost(request,response);
+    }
+    public void endSoapRequest(WebResponse theResponse){
+
+    }
+*/
+}
